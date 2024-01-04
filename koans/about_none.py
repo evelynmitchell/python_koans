0515246611b728e501afea7a3240b544ff4f9fae
evelynmitchell <efmphone@gmail.com>
2024-01-04 08:12:47 -0700

    Koan 28 redo 2nd part

--- koans/about_none.py
+++ koans/about_none.py
@@ -41,7 +41,7 @@ class AboutNone(Koan):
 
         # What message was attached to the exception?
         # (HINT: replace __ with part of the error message.)
-        self.assertRegex(ex2.args[0], 'AttributeError') 
+        self.assertRegex(ex2.args[0], 'attribute')
 
     def test_none_is_distinct(self):
         """
