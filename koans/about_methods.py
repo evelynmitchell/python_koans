d519b33094605c5c04d3a3333a1e13965ad15fdc
evelynmitchell <efmphone@gmail.com>
2024-01-04 11:25:26 -0700

    Koan 85 typo

--- koans/about_methods.py
+++ koans/about_methods.py
@@ -126,7 +126,7 @@ class AboutMethods(Koan):
         return "ok"
 
     def test_the_documentation_can_be_viewed_with_the_doc_method(self):
-        self.assertRegex(self.method_with_documentation.__doc__, 'ok'A string placed at the beginning of a function is used for documentation')
+        self.assertRegex(self.method_with_documentation.__doc__, 'A string placed at the beginning of a function is used for documentation')
 
     # ------------------------------------------------------------------
 
