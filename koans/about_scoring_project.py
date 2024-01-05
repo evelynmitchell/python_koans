8d79356345528903527f58a30f8fb089fd2d8f0f
evelynmitchell <efmphone@gmail.com>
2024-01-05 15:28:46 -0700

    Koan 152 try

--- koans/about_scoring_project.py
+++ koans/about_scoring_project.py
@@ -40,8 +40,16 @@ def score(dice):
         score += 50
     if dice.count(1) == 1:
         score += 100
-        return score
-    
+    if dice.count(1) == 3:
+        score += 1000
+    if dice.count(2) == 3:
+        score += 200
+    if dice.count(3) == 3:
+        score += 300
+    if dice.count(4) == 3:
+        score += 400
+    if dice.count(5) == 3:
+        score += 500
 
     
 class AboutScoringProject(Koan):
