ff9b0cae3ee01313444b5abb3ec2c143a2b17872
evelynmitchell <efmphone@gmail.com>
2024-01-05 13:00:09 -0700

    Koan 145 146

--- koans/about_generators.py
+++ koans/about_generators.py
@@ -132,11 +132,11 @@ class AboutGenerators(Koan):
 
         generator2 = self.yield_tester()
         next(generator2)
-        self.assertEqual(__, next(generator2))
+        self.assertEqual('no value', next(generator2))
 
     def test_send_none_is_equivalent_to_next(self):
         generator = self.yield_tester()
 
         next(generator)
         # 'next(generator)' is exactly equivalent to 'generator.send(None)'
-        self.assertEqual(__, generator.send(None))
+        self.assertEqual(None, generator.send(None))
