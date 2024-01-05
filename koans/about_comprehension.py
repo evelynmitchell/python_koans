9ffae131a690f934aaa1ffb515b97689b80bad55
evelynmitchell <efmphone@gmail.com>
2024-01-05 10:54:06 -0700

    Koan 135 thinko fix

--- koans/about_comprehension.py
+++ koans/about_comprehension.py
@@ -55,7 +55,7 @@ class AboutComprehension(Koan):
 
         dict_comprehension = { k.upper(): weapon for k, weapon in dict_of_weapons.items() if weapon}
 
-        self.assertEqual(None, 'first' in dict_comprehension)
+        self.assertEqual(False, 'first' in dict_comprehension)
         self.assertEqual('fear', 'FIRST' in dict_comprehension)
         self.assertEqual(5, len(dict_of_weapons))
         self.assertEqual(4, len(dict_comprehension))
