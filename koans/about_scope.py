bdeae4382be335939ca0193248064e9227e34c56
evelynmitchell <efmphone@gmail.com>
2024-01-07 15:36:59 -0700

    Koan 230

--- koans/about_scope.py
+++ koans/about_scope.py
@@ -48,13 +48,13 @@ class AboutScope(Koan):
     PI = 3.1416
 
     def test_constants_are_defined_with_an_initial_uppercase_letter(self):
-        self.assertAlmostEqual(_____, self.PI)
+        self.assertAlmostEqual(3.1416, self.PI)
         # Note, floating point numbers in python are not precise.
         # assertAlmostEqual will check that it is 'close enough'
 
     def test_constants_are_assumed_by_convention_only(self):
         self.PI = "rhubarb"
-        self.assertEqual(_____, self.PI)
+        self.assertEqual('rhubarb', self.PI)
         # There aren't any real constants in python. Its up to the developer
         # to keep to the convention and not modify them.
 
