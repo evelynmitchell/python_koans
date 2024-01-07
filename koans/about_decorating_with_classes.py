b538cdcd278d2d7c748d7aabf0fb907156a0e462
evelynmitchell <efmphone@gmail.com>
2024-01-07 14:49:55 -0700

    Koan 207 thinko

--- koans/about_decorating_with_classes.py
+++ koans/about_decorating_with_classes.py
@@ -108,7 +108,7 @@ class AboutDecoratingWithClasses(Koan):
         pass
 
     def test_decorator_with_an_argument(self):
-        self.assertEqual(2, self.count_badly(2))
+        self.assertEqual(5, self.count_badly(2))
         self.assertEqual("Increments a value by one. Kind of.", self.count_badly.__doc__)
 
     def test_documentor_which_already_has_a_docstring(self):
