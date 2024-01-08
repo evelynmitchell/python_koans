f09e47c056765d615901a7bb57e07df9f4554012
evelynmitchell <efmphone@gmail.com>
2024-01-08 10:43:43 -0700

    Koan 274 fix

--- koans/about_attribute_access.py
+++ koans/about_attribute_access.py
@@ -135,7 +135,7 @@ class AboutAttributeAccess(Koan):
         catcher = self.MinimalCatcher()
         catcher.my_method()
 
-        self.assertEqual(2, catcher.no_of_getattr_calls)
+        self.assertEqual(0, catcher.no_of_getattr_calls)
 
     def test_getattr_only_catches_unknown_attributes(self):
         catcher = self.MinimalCatcher()
