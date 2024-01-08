abd3e732fa34e3d806fe26746655ecb500d9bd13
evelynmitchell <efmphone@gmail.com>
2024-01-08 10:29:03 -0700

    Koan 268, 269 270 attribute access

--- koans/about_attribute_access.py
+++ koans/about_attribute_access.py
@@ -20,7 +20,7 @@ class AboutAttributeAccess(Koan):
     def test_calling_getattribute_causes_an_attribute_error(self):
         typical = self.TypicalObject()
 
-        with self.assertRaises(___): typical.__getattribute__('foobar')
+        with self.assertRaises(AttributeError): typical.__getattribute__('foobar')
 
         # THINK ABOUT IT:
         #
@@ -36,7 +36,7 @@ class AboutAttributeAccess(Koan):
     def test_all_attribute_reads_are_caught(self):
         catcher = self.CatchAllAttributeReads()
 
-        self.assertRegex(catcher.foobar, __)
+        self.assertRegex(catcher.foobar, "Someone called 'foobar' and it could not be found")
 
     def test_intercepting_return_values_can_disrupt_the_call_chain(self):
         catcher = self.CatchAllAttributeReads()
