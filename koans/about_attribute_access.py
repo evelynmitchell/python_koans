da254a2af7e8ddc4e3b71585c5c4c316e0f1ab7b
evelynmitchell <efmphone@gmail.com>
2024-01-08 10:45:16 -0700

    Koan 276, 277

--- koans/about_attribute_access.py
+++ koans/about_attribute_access.py
@@ -142,10 +142,10 @@ class AboutAttributeAccess(Koan):
         catcher.purple_flamingos()
         catcher.free_pie()
 
-        self.assertEqual(__,
+        self.assertEqual('MinimalCatcher',
             type(catcher.give_me_duff_or_give_me_death()).__name__)
 
-        self.assertEqual(__, catcher.no_of_getattr_calls)
+        self.assertEqual(1, catcher.no_of_getattr_calls)
 
     # ------------------------------------------------------------------
 
