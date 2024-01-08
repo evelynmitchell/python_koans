b67d1a4ef08d47302c7ed1fdf08cd982420c215b
evelynmitchell <efmphone@gmail.com>
2024-01-08 15:17:30 -0700

    Koan 288 undo

--- koans/about_proxy_object_project.py
+++ koans/about_proxy_object_project.py
@@ -29,8 +29,15 @@ class Proxy:
 
     # WRITE CODE HERE
     def __getattr__(self, attr_name):
-        self._messages.append(attr_name)
-        return getattr(self._obj, attr_name)
+        if attr_name == 'power':
+            return self._obj.power()
+        elif attr_name == 'channel':
+            return self._obj.channel
+        elif attr_name == 'is_on':
+            self._obj.is_on()
+        else:
+            self._messages.append(attr_name)
+            return getattr(self._obj, attr_name)
 
     def __setattr__(self, attr_name, value):
         if attr_name == '_obj':
