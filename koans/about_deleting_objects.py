3f91fd49a4c05257a5ffa7f1661f252ba7031ff9
evelynmitchell <efmphone@gmail.com>
2024-01-08 11:11:35 -0700

    Koan 286 fix

--- koans/about_deleting_objects.py
+++ koans/about_deleting_objects.py
@@ -119,6 +119,6 @@ class AboutDeletingObjects(Koan):
 
     def tests_del_can_be_overriden(self):
         sale = self.MoreOrganisedClosingSale()
-        self.assertEqual(AttributeError, sale.jellies())
+        self.assertEqual(5, sale.jellies())
         del sale.jellies
         self.assertEqual('jellies', sale.last_deletion)
